f3ee05593650863ac6a4be5d717902f167c5b9eb
NamMinwu <minwoo000625@naver.com>
2025-06-19 15:04:13 +0900

    FEAT : setting for ci cd

--- src/main/java/kubernetes_github_action/ci_cd/core/Main.java
+++ src/main/java/kubernetes_github_action/ci_cd/core/Main.java
@@ -1,5 +1,15 @@
 package kubernetes_github_action.ci_cd.core;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class Main {
 
+  @GetMapping("/")
+  public String main() {
+    return "main";
+  }
 }
